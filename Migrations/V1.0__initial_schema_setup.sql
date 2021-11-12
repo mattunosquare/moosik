40f169e1b5fc9de4d019bfb4abd93ebd32e751d9
Matt McCleave <matt.mccleave@unosquare.com>
2021-11-12 12:30:36 +0000

    Adding initial migrations

---
+++ Migrations/V1.0__initial_schema_setup.sql
@@ -0,0 +1,78 @@
+CREATE TABLE IF NOT EXISTS public.thread_types
+(
+    id serial constraint thread_type_pk primary key,
+    description text
+);
+
+CREATE TABLE IF NOT EXISTS public.resource_types
+(
+    id  serial constraint resource_type_pk primary key,
+    description text
+);
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id  serial constraint user_pk primary key,
+    username text,
+    email text
+);
+
+CREATE TABLE IF NOT EXISTS public.threads
+(
+    id  serial constraint thread_pk primary key,
+    title text,
+    created_date timestamptz
+);
+
+ALTER TABLE public.threads ADD COLUMN thread_type_id INTEGER;
+ALTER TABLE public.threads
+    ADD CONSTRAINT thread_type_fk
+    FOREIGN KEY (thread_type_id)
+    REFERENCES public.thread_types(id);
+
+ALTER TABLE public.threads add column user_id INTEGER;
+ALTER TABLE public.threads
+    add constraint user_fk
+    foreign key (user_id)
+    references public.users(id);
+
+CREATE TABLE IF NOT EXISTS public.posts
+(
+    id serial constraint post_pk primary key,
+    description text,
+    created_date timestamptz
+);
+
+ALTER TABLE public.posts add column user_id INTEGER;
+alter Table public.posts
+    add constraint user_fk
+    foreign key (user_id)
+    references public.users(id);
+
+alter table public.posts add column thread_id INTEGER;
+alter table public.posts
+    add constraint thread_fk
+    foreign key (thread_id)
+    references public.threads(id);
+
+
+
+CREATE TABLE IF NOT EXISTS public.post_resources
+(
+    id serial constraint post_resources_pk primary key,
+    title text,
+    value text
+);
+
+alter table public.post_resources add column post_id INTEGER;
+alter table public.post_resources
+    add constraint post_fk
+    foreign key (post_id)
+    references public.posts(id);
+
+alter table public.post_resources add column resource_type_id INTEGER;
+alter table public.post_resources
+    add constraint resource_type_fk
+    foreign key (resource_type_id)
+    references public.resource_types(id);    
+
---
+++ Migrations/V1.1__seeding_data.sql
@@ -0,0 +1,64 @@
+/*ThreadTypes Table*/
+INSERT INTO public.thread_types (description) VALUES ('Request');
+INSERT INTO public.thread_types (description) VALUES ('Recommendation');
+
+/*ResourceTypes Table*/
+INSERT INTO public.resource_types (description) VALUES ('Hyperlink');
+INSERT INTO public.resource_types (description) VALUES ('Embedded Spotify');
+
+/*Users Table*/
+INSERT INTO public.users (username, email) VALUES ('pespinosa0', 'jerickson0@webs.com');
+INSERT INTO public.users (username, email) VALUES ('anesbeth1', 'hfrancesco1@discovery.com');
+INSERT INTO public.users (username, email) VALUES ('nalves2', 'cmcelvogue2@altervista.org');
+INSERT INTO public.users (username, email) VALUES ('vreveland3', 'emcnea3@tripod.com');
+INSERT INTO public.users (username, email) VALUES ('dtrodler41', 'ariseame@soup.io');
+INSERT INTO public.users (username, email) VALUES ('hbowcher5', 'fmarcosc@chronoengine.com');
+INSERT INTO public.users (username, email) VALUES ('mirdaleb', 'asifflettq@vimeo.com');
+INSERT INTO public.users (username, email) VALUES ('amorewoodi', 'zarchibouldj@google.com.au');
+INSERT INTO public.users (username, email) VALUES ('laltonp', 'ebootelll@1688.com');
+INSERT INTO public.users (username, email) VALUES ('cmcmeylerr', 'hfrancesco1@discovery.com');
+
+/*Threads Table*/
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Help!', 1, 1, '20210116 11:13:00 AM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Gym', 1, 2, '20210119 08:12:00 AM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Working', 2, 4, '20210401 09:11:00 PM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Going Camping', 2, 3, '20210304 06:00:00 PM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Holidays', 1, 5, '20210218 04:10:00 PM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Going to Gym', 1, 6, '20210720 11:15:00 PM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Bored', 2, 7, '20210914 10:20:00 AM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('On the train', 2, 9, '20211215 10:38:00 AM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('Whats Up', 1, 9, '20210125 10:42:00 PM');
+INSERT INTO public.threads (title, thread_type_id, user_id, created_date) VALUES ('NVM', 1, 9, '20210721 10:54:00 AM');
+
+/*Posts Table*/
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Need some more Beatles music please', 7, 1 ,'20210116 11:13:00 AM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Great beat for gym',4,2,  '20210119 08:12:00 AM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Need some music for programming please',3,3,  '20210401 09:11:00 PM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Good camping music',9,4,  '20210304 06:00:00 PM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Need music for holidays please', 3,5, '20210218 04:10:00 PM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Need music for cardio please', 6,6, '20210720 11:15:00 PM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('I like this song when I am bored',9,7,  '20210914 10:20:00 AM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Good wee song for being on the train',10,8,'20211215 10:38:00 AM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Whats up?',2,9, '20210125 10:42:00 PM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Nevermind', 1, 10, '20210721 10:54:00 AM');
+
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Hey, I listened to this song, it sounded great!', 2, 2, '20210119 11:12:00 AM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('I love yellow submarine, check it out', 1, 1, '20210116 04:13:00 PM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Check out help, great song', 1, 1, '20210119 06:12:00 PM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('Try this some while running at gym', 4, 6, '20210119 08:48:00 AM');
+INSERT INTO public.posts(description,user_id, thread_id, created_date) VALUES ('I also like this song when I am bored', 7, 7, '20210119 08:55:00 AM');
+
+/*PostResources Table*/
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (1,1,'Hey Jude', 'youtube.com/heyjude');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (2,1, 'Tiesto', 'youtube.com/adagioforstrings');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (4,2, 'All These Things Ive Done', 'iframecode01');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (5,1, 'Fat Lip', 'youtube.com/fatlip');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (6,2, 'I will wait', 'iframecode02');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (7,2, 'Ho Hey', 'iframecode03');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (8,2, 'Hotline Bling', 'iframecode04');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (11,2, 'Feeling This', 'iframecode05');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (12,1, 'Yellow', 'youtube.com/yellow');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (13,2, 'You Make My Dreams(Come True)', 'iframecode06');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (14,1, 'In The Air Tonight', 'youtube.com/intheairtonight');
+INSERT INTO public.post_resources (post_id, resource_type_id, title, value) values (15,2, 'My Sharona', 'iframecode07');
+
